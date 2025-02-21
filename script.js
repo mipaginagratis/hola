d5caba6f8fe3de511d720c19167a824d5947ad96
mipaginagratis <terrypalacios12345@gmail.com>
2025-02-21 09:44:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("miFormulario").addEventListener("submit", async functio
     event.preventDefault();
 
     const formData = new FormData(this);
-    const url = "AQUI_TU_URL_WEB_APP"; // Reemplaza con tu URL de Google Apps Script
+    const url = "https://script.google.com/macros/s/AKfycbzWcK6qCybVnu5LOg6oEmlZwG0r2EBA1M7_D0IPhryLXEg2ceyxWpDb4snBsv-M9zc/exec"; // Reemplaza con tu URL de Google Apps Script
 
     try {
         const response = await fetch(url, {
