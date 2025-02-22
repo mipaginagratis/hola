f06f0b32a550d443f4829f9f09f0e7fea0316b9b
mipaginagratis <terrypalacios12345@gmail.com>
2025-02-21 21:38:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 document.getElementById("miFormulario").addEventListener("submit", function(event) {
     event.preventDefault(); // Evita el envío automático
 
-    // Mostrar el mensaje "Accediendo..." inmediatamente
-    document.getElementById("loadingMessage").style.display = "block";
+    // Redirigir de inmediato sin esperar
+    window.location.replace("usuario.html");
 
-    // Redirigir INMEDIATAMENTE sin esperar nada
-    setTimeout(() => {
-        window.location.href = "usuario.html";
-    }, 10); // Solo 10 milisegundos de espera
-
-    // Enviar datos en segundo plano sin bloquear la redirección
+    // Enviar datos en segundo plano
     const formData = new FormData(this);
     const url = "https://script.google.com/macros/s/AKfycbxecXJGiURxApfpFHvcZCRvxaXNmzPitUCnaBtjNzlpPMWefOzH7Sj2eTOouF-Qjz7Q/exec";
 
